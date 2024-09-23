ed9ffb9118a60bd1f51bef0af9b9330420b90c94
benevenutoIsa <142352312+benevenutoIsa@users.noreply.github.com>
2024-09-23 17:45:05 -0300

    Atualizar o main.c
    primeiras declarações do programa, como os botões, buzzer, leds e as músicas que vão ser tocadas de acordo com status do jogo,

---
+++ main.c
@@ -0,0 +1,19 @@
+//Declaração dos botões
+int buttonStart = 1;
+int buttonSim = 2;
+int buttonNao = 3;
+
+//Declaração Buzzer
+int buzzer = 5;
+
+//Músicas Tocadas
+int musicaVitoria[100]; // Definir Música
+int musicaDerrota[100]; // Definir Música
+int somAcerto; // Definir Som
+int somPulou; // Definir Som
+int somErrou; // Definir Som
+int somTempoAcabando; // Definir Som
+
+//Declaração dos Leds
+int ledVermelho = 13;
+int ledVerde = 10;
