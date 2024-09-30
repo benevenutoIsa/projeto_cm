f53e14b9e24d5f8789c44936a4e94bd305531703
Marana <assumateus@gmail.com>
2024-09-30 19:38:13 -0300

    features[pertguntas e respostas]; fixed[FasePerguntas e FasePerguntasFinal]

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <LiquidCrystal.h>
+char buffer[3];
 //Declaração dos botões
 int buttonStart = 5;
 int buttonSim = 3;
@@ -6,43 +8,56 @@ int buttonNao = 9;
 //Declaração Buzzer
 int buzzer = 2;
 
-//Músicas Tocadas
-int somPulou; // Definir Som
-
 //Declaração dos Leds
 int ledVermelho = 13;
 int ledVerde = 10;
 
 // Questões
-String questoesBasicas[10];
-String respostasCorretas[10];
+const char* questoesBasicas[] = {
+    "1+1=2", "3*2=6", "7-5=3", "10/2=5", "4+9=13", "15-7=8", 
+    "12/3=4", "5*6=30", "9+6-4=11", "8*3=24"
+};
+const char* respostasCorretas[] = {
+    "SIM", "SIM", "NAO", "SIM", "SIM", "NAO", "SIM", "SIM", 
+    "SIM"
+};
 
 //Pergunta Final
-String questoesFinais[5];
-String respostasFinaisCorretas[5];
+const char* questoesFinais[] = {
+	"2^4=16", "√144=12", "5+(3*2)=11", "20-3*4+2=10", "3*2+4*2=47"
+};
+String respostasFinaisCorretas[] = {
+	"SIM", "SIM", "SIM", "NAO", "NAO"
+};
 
 // Jogo da Memória
 int armazenaValores[10];
 int respostasUsuarioMemoria[10];
 
+// Inicializa os objetos lcd
+LiquidCrystal lcd(12, 11, 8, 7, 6, 4);
+
 //Vida
-int vida = 1;
+int vida = 2;
 
 bool Temporizador(){
-	int contador;
-  for(contador = 30; contador > 0; contador--){
-  	Serial.println(contador);
-    delay(1000);
-    if(contador <= 5){
-		tone(buzzer,440);    
+    int contador;
+    for(contador = 30; contador > 0; contador--){
+        Serial.println(contador);
+        sprintf(buffer, "%02d", contador);
+        lcd.setCursor(14, 1);
+        lcd.print(buffer);
+        delay(1000);
+        if(contador <= 5){
+            tone(buzzer, 440);    
+        }
+        if (digitalRead(buttonSim) == LOW || digitalRead(buttonNao) == LOW) {
+            noTone(buzzer); 
+            return true; 
+        }
     }
-     if (digitalRead(buttonSim) == LOW || digitalRead(buttonNao) == LOW) {
-      noTone(buzzer); 
-      return true; 
-    }
-  }
-  noTone(buzzer);
-  return false;
+    noTone(buzzer);
+    return false;
 }
 
 void SomAcerto(int buzzer){
@@ -74,7 +89,7 @@ void MusicaDerrota(int buzzer){
 }
 
 void MusicaVitoria(int buzzer){
-    delay(2000);
+  Serial.println("A");
     tone(buzzer, 392, 300); //SOL
     delay(350);
     tone(buzzer, 392, 300); //SOL
@@ -88,12 +103,12 @@ void MusicaVitoria(int buzzer){
 void AcendeLeds(){
 	for(int i = 0; i < 10; i++){
       	int randomNumber = random(0,2);
-		if(randomNumber == 0){
+		if(randomNumber == 1){
           digitalWrite(ledVermelho, HIGH);
           digitalWrite(ledVerde,LOW);
           armazenaValores[i] = 0;
           delay(500);
-        	digitalWrite(ledVermelho,LOW);
+		  digitalWrite(ledVermelho,LOW);
 		} else {
           digitalWrite(ledVermelho,LOW);
           digitalWrite(ledVerde,HIGH);
@@ -107,6 +122,7 @@ void AcendeLeds(){
 
 void VerificaMemoria(){
   int contador = 0;
+  lcd.clear();
 	do{
       Serial.println(contador);
 		if(digitalRead(buttonSim) == LOW){
@@ -123,8 +139,11 @@ void VerificaMemoria(){
 }
 
 bool SequenciaEstaCerta(){
+	lcd.clear();
   for(int i = 0; i<10 ;i++){
 		if(respostasUsuarioMemoria[i] != armazenaValores[i]){
+			lcd.setCursor(0,0);
+			lcd.print("Sequencia Incorreta");
 			return false;
 		}
 	}
@@ -133,62 +152,115 @@ bool SequenciaEstaCerta(){
 
 
 int FasePerguntas(){
-  int randomNumber;
-  for (int i = 0; i < 5; i++) {	
-    String respostaUsuario;
-    randomNumber = random(0, 10);
-    String pergunta = questoesBasicas[randomNumber];
-    String respostaCorreta = respostasCorretas[randomNumber]; 
-    if(Temporizador()){
-      if (digitalRead(buttonSim) == LOW) {
-        respostaUsuario = "Sim";
-      } else if (digitalRead(buttonNao) == LOW) {
-        respostaUsuario = "Não";
-      }
+    int randomNumber;
+    for (int i = 0; i < 5; i++) {    
+        String respostaUsuario;
+        randomNumber = random(0, 9); 
+        String respostaCorreta = respostasCorretas[randomNumber];
+        
+        lcd.setCursor(0, 0);
+        lcd.print(questoesBasicas[randomNumber]);
+        
+        delay(1000);
+        if(Temporizador()){
+            if (digitalRead(buttonSim) == LOW) {
+                respostaUsuario = "SIM";
+            } else if (digitalRead(buttonNao) == LOW) {
+                respostaUsuario = "NAO";
+            }
 
-      if (!respostaUsuario.equals(respostaCorreta)) {
-        vida--;
-        SomErro(buzzer);
-        if(vida<=0){
-        	MusicaDerrota(buzzer);
-          	return 0;
+            if (!respostaUsuario.equals(respostaCorreta)){
+                lcd.clear();
+                lcd.setCursor(0, 0);
+                lcd.print("Resposta Errada");
+                vida--;
+                SomErro(buzzer);
+                delay(2000);
+                lcd.clear();
+                if(vida <= 0){
+                    MusicaDerrota(buzzer);
+                    lcd.clear();
+                    lcd.setCursor(0, 0);
+                    lcd.print("Fim de Jogo");
+                    delay(2000);
+                    lcd.clear();
+                    return 0;
+                }
+            } else {
+                lcd.clear();
+                lcd.setCursor(0, 0);  
+                lcd.print("Resposta Certa");
+                SomAcerto(buzzer);
+              	delay(1000);
+              	lcd.clear();
+            }
+        } else {
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("Tempo Esgotado");
+            vida--;
+            SomPulou(buzzer);
+            delay(2000);
+            lcd.clear();
+            if(vida <= 0){
+                lcd.clear();
+                lcd.setCursor(0, 0);  
+                lcd.print("Fim de Jogo");
+                MusicaDerrota(buzzer);
+                delay(2000);
+                lcd.clear();
+                return 0;
+            }
         }
-      } else {
-        SomAcerto(buzzer);
-      }
-    } else {
-      	vida--;
-    	SomPulou(buzzer);
-      if(vida <=0){
-      	MusicaDerrota(buzzer);
-      }
+        delay(500);  
     }
-    delay(500);  
-  }
-  return 1; 
+    return 1; 
 }
 
 
 int FasePerguntaFinal(){
   int randomNumber;
   String respostaUsuario;
-  randomNumber = random(0, 5);
-  String pergunta = questoesFinais[randomNumber];
+  randomNumber = random(0, 4);
+  
+  lcd.setCursor(0,0);
+  lcd.print("Você Chegou na Pergunta Final");
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(questoesFinais[randomNumber]);
+  
   String respostaCorreta = respostasFinaisCorretas[randomNumber];
+  delay(1000);
   if(Temporizador()){
     if (digitalRead(buttonSim) == LOW) {
-      respostaUsuario = "Sim";
+      respostaUsuario = "SIM";
     } else if (digitalRead(buttonNao) == LOW) {
-      respostaUsuario = "Não";
+      respostaUsuario = "NAO";
     }
     if (!respostaUsuario.equals(respostaCorreta)) {
-      MusicaDerrota(buzzer);
+      lcd.clear();
+	  lcd.setCursor(0,0);
+	  lcd.print("Resposta Errada");
+	  MusicaDerrota(buzzer);
+      delay(1000);
+      lcd.clear();
+      lcd.print("Fim de Jogo");
       return 0;  
     } else {
+		lcd.clear();
+		lcd.setCursor(0,0);
+		lcd.print("Resposta Certa");
     	MusicaVitoria(buzzer);
+      	delay(1000);
+      	lcd.clear();
+      	lcd.print("Parabéns você ganhou!!!");
     }
   } else {
+	lcd.clear();
+	lcd.setCursor(0,0);
+	lcd.print("Tempo Esgotado");
   	MusicaDerrota(buzzer);
+	return 0;
   }
   return 1; 
 }
@@ -203,7 +275,8 @@ pinMode(2,OUTPUT); // Buzzer
 pinMode(13,OUTPUT); // Led Vermelho
 pinMode(10,OUTPUT); // Led Verde
   
-Serial.begin(9600);
+Serial.begin(9600); // Inicia o Serial
+lcd.begin(16,2); //Inicia o Lcd
 
 }
 
@@ -211,14 +284,13 @@ void loop() {
   if (digitalRead(buttonStart) == LOW) {
     delay(500);
 
-    vidas = 2;
-
     AcendeLeds();
     VerificaMemoria();
     
     if (SequenciaEstaCerta()) {
       MusicaVitoria(buzzer);
-      Serial.println("Você acertou a sequência!");
+      lcd.clear();
+	  lcd.setCursor(0,0);
 
       // Iniciar fase de perguntas
       if (FasePerguntas() == 1) {
@@ -229,7 +301,9 @@ void loop() {
       }
     } else {
       MusicaDerrota(buzzer);
-      Serial.println("Você errou a sequência.");
+      lcd.clear();
+	  lcd.setCursor(0,0);
+	  Serial.println("Você errou a sequência.");
     }
   }
 }
